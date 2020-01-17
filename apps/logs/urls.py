5ad831d7efe2ba59fc60fec1f63a7a814d9df2c7
胡绪沙 <huxusha@jczh56.com>
2020-01-17 16:49:20 +0800

    日志模块路由配置文件

--- apps/logs/urls.py
+++ apps/logs/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from . import views
-app_name = 'logs'
-urlpatterns = [
 
+
+app_name = 'logs'
+
+urlpatterns = [
+    url("log_info/", views.LogsView.as_view(), name="log_info"),    # 日志信息显示
 ]
