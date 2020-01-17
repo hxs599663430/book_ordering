50ad7e25281dab016dbd29c9bf44d69188462eb2
胡绪沙 <huxusha@jczh56.com>
2020-01-17 16:48:42 +0800

    日志模块序列化文件

---
+++ apps/logs/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from logs.models import Logs
+
+
+class LogsSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Logs
+        fields = "__all__"
